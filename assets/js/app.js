b3f7d358c8fd797c12098d05c34ff6df1ac5b8a3
asifulmamun <asifulmamun@gmail.com>
2021-02-11 22:00:30 +0600

    home page ok but not responsive

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,20 @@
 // Main Js
+
+// /* 
+//     ------- Giving Rating JS
+// */
+// function ratingOne(){
+//     ratingTwoStop();
+//     document.getElementById('ratingOne').innerHTML = '<i style="color:#F9E370;" class="fas fa-star"></i>';
+//     // do anything
+// }
+// function ratingTwo(){
+//     ratingOne();
+//     document.getElementById('ratingTwo').innerHTML = '<i style="color:#F9E370;" class="fas fa-star"></i>';
+//     // do anything
+// }
+// function ratingTwoStop(){
+//     document.getElementById('ratingTwo').innerHTML = '<i class="far fa-star"></i>';
+// }
+
+
