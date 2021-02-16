6e761c1d8856afc84f87e04d146522233028cc3d
asifulmamun <asifulmamun@gmail.com>
2021-02-16 20:48:03 +0600

    short by button ok need to styling

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,20 @@
-// Main Js
+/* 
+    --------- Menu
+*/
+// insert menu icon to a div
+var main_menu_icon = '<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="bars" class="svg-inline--fa fa-bars fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"></path></svg>';
+document.getElementById('main_menu_icon').innerHTML = main_menu_icon;
 
-// /* 
-//     ------- Giving Rating JS
-// */
-// function ratingOne(){
-//     ratingTwoStop();
-//     document.getElementById('ratingOne').innerHTML = '<i style="color:#F9E370;" class="fas fa-star"></i>';
-//     // do anything
-// }
-// function ratingTwo(){
-//     ratingOne();
-//     document.getElementById('ratingTwo').innerHTML = '<i style="color:#F9E370;" class="fas fa-star"></i>';
-//     // do anything
-// }
-// function ratingTwoStop(){
-//     document.getElementById('ratingTwo').innerHTML = '<i class="far fa-star"></i>';
-// }
+// menu click function
+document.getElementById('main_menu_icon').addEventListener('click', function(){
 
+    if(document.getElementById('main_menu_ul_id').style.display === 'block'){
 
+        document.getElementById('main_menu_ul_id').style.display = 'none';
+
+    } else{
+
+        document.getElementById('main_menu_ul_id').style.display = 'block'
+        
+    }
+});/* menu click function */
